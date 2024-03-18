18da4b224e517d713883de80ae444f5b6620e3f0
tinnan <tin_nan@hotmail.com>
2024-03-18 12:18:23 +0700

    To make feign client automatically load balances request -> name attribute of @FeignClient must match application name registered to Eureka server and do not set url attribute to @FeignClient

--- clients/src/main/java/com/example/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/example/clients/fraud/FraudClient.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name = "fraud",
-        url = "${clients.fraud.url}"
+        name = "fraud"
+//        url = "${clients.fraud.url}"
 )
 public interface FraudClient {
 
--- customer/src/main/java/com/example/customer/CustomerApplication.java
+++ customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -2,6 +2,7 @@ package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
