d6e18c73f0d463bc73e047155b199f80b5c9efbb
tinnan <tin_nan@hotmail.com>
2024-03-26 22:20:58 +0700

    Reduce code redundant.

--- fraud/src/main/java/com/example/fraud/FraudController.java
+++ fraud/src/main/java/com/example/fraud/FraudController.java
@@ -1,22 +1,23 @@
 package com.example.fraud;
 
 import com.example.clients.fraud.FraudCheckResponse;
+import com.example.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
 @Slf4j
-public class FraudController {
+public class FraudController implements FraudClient {
 
     private final FraudCheckService fraudCheckService;
 
-    @GetMapping(path = "{customerId}")
-    public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
+    @Override
+    public FraudCheckResponse isFraudster(Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
         log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
+
 }
